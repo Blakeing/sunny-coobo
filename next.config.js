ab7df997cb31593e1dfa8cb5a9db80a7a4771cc6
Blake Ingenthron <blakeingenthron@gmail.com>
2023-02-02 19:30:13 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,4 +8,12 @@ module.exports = {
   images: {
     domains: ['images.unsplash.com', 'cdn.sanity.io'],
   },
+  typescript: {
+    // Set this to false if you want production builds to abort if there's type errors
+    ignoreBuildErrors: process.env.VERCEL_ENV === 'production',
+  },
+  eslint: {
+    /// Set this to false if you want production builds to abort if there's lint errors
+    ignoreDuringBuilds: process.env.VERCEL_ENV === 'production',
+  },
 }
